733419e084530940aebc5fde2eaf6b57abe0d520
Zahid Manzoor <zahidmanzoor2235@gmail.com>
2021-01-07 15:15:00 +0500

    assignment folder is created

---
+++ Assignment 5.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+
+public class BinarySearch {
+
+
+    private BinarySearch() { }
+
+
+    public static int indexOf(int[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    public static int rank(int key, int[] a) {
+        return indexOf(a, key);
+    }
+
+
+    public static void main(String[] args) {
+
+        // read the integers from a file
+        In in = new In(args[0]);
+        int[] allowlist = in.readAllInts();
+
+        // sort the array
+        Arrays.sort(allowlist);
+
+        // read integer key from standard input; print if not in allowlist
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (BinarySearch.indexOf(allowlist, key) == -1)
+                StdOut.println(key);
+        }
+    }
+}
