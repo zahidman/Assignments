c6aa25dcae85237ce6a77a4dbf4ea5cecd4d9c0c
Zahid Manzoor <zahidmanzoor2235@gmail.com>
2021-01-10 10:54:59 +0500

    new Assignment

---
+++ Assignment 5(a).java
@@ -0,0 +1,14 @@
+// Delaration and assignment operation
+ public class CalculateMoney
+ {
+   public static void main(String[] args)
+   {
+     int numDimes = 7;
+     int numQuarters = 3;
+     int numNickels = 8;
+
+     int total = numDimes * 10 + numQuarters + 25;
+
+     System.out.println("Total = " + total);
+   }
+ }
